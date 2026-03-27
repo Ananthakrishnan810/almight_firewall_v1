620999a9a07b12f547180bc7f41c1facce0c5c88
ananthakpm <ananthakpm@am.amrita.edu>
2026-03-27 15:02:04 +0530

    added the allow,block and the python ip address into the constantInUse file

--- src/main/java/com/example/firewall/utils/ConstantsInUse.java
+++ src/main/java/com/example/firewall/utils/ConstantsInUse.java
@@ -3,8 +3,12 @@ package com.example.firewall.utils;
 public class ConstantsInUse {
  
     public static String URL_USE = "http://localhost:7080/auth";
+    public static String PYTHON_CONNECTION = "http://localhost:5000";
+    public static String DETECT_API = "/detect";
     public static String ENCODEDQUERY = "id=1%20OR%201=1";
     public static String ENCODEDXSS = "%3Cscript%3Ealert('XSS')%3C%2Fscript%3E";
     public static String ENCODEDPATH = "..%2F..%2F..%2Fetc%2Fpasswd";
+    public static String ALLOW_THE_REQUEST = "ALLOW";
+    public static String BLOCK_THE_REQUEST = "BLOCK";
 
 }
