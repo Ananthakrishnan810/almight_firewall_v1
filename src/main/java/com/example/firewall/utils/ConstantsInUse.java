49e946dc3351335b2518718bd08b561617dffcc2
ananthakpm <ananthakpm@am.amrita.edu>
2026-03-27 13:59:50 +0530

    captilize the words in the constantInUse file

--- src/main/java/com/example/firewall/utils/ConstantsInUse.java
+++ src/main/java/com/example/firewall/utils/ConstantsInUse.java
@@ -2,9 +2,9 @@ package com.example.firewall.utils;
 
 public class ConstantsInUse {
  
-    public static String url_user = "http://localhost:7080/auth";
-    public static String encodedQuery = "id=1%20OR%201=1";
-    public static String encodedXSS = "%3Cscript%3Ealert('XSS')%3C%2Fscript%3E";
-    public static String encodedPath = "..%2F..%2F..%2Fetc%2Fpasswd";
+    public static String URL_USE = "http://localhost:7080/auth";
+    public static String ENCODEDQUERY = "id=1%20OR%201=1";
+    public static String ENCODEDXSS = "%3Cscript%3Ealert('XSS')%3C%2Fscript%3E";
+    public static String ENCODEDPATH = "..%2F..%2F..%2Fetc%2Fpasswd";
 
 }
