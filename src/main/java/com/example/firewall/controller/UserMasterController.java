76cd4048c5da6b9622782145a43925e14650f5fb
Ananthakrishna810 <ananthakrishna810@gmail.com>
2026-03-31 23:43:25 +0530

    created three moreapis as the first one is for fetching the userDetails of cthe current euser and xthe second one is for fetching dthe all created users that are active now and the thrid one is for deleting the active users

--- src/main/java/com/example/firewall/controller/UserMasterController.java
+++ src/main/java/com/example/firewall/controller/UserMasterController.java
@@ -1,13 +1,21 @@
 package com.example.firewall.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.firewall.entity.UserMasterEntity;
 import com.example.firewall.pojo.UserMasterPojo;
 import com.example.firewall.service.AuthService;
+
 
 @RestController
 @RequestMapping("/users")
@@ -16,10 +24,36 @@ public class UserMasterController {
     @Autowired
     private AuthService authService;
 
+
     @PostMapping("/register")
     public String registerUser(@RequestBody UserMasterPojo user){
 
         return authService.register(user);
         
     }
+
+    @GetMapping("/getAllUsers")
+    public ResponseEntity<?> fetchUser(@RequestBody UserMasterPojo user) {
+        
+        return ResponseEntity.ok(authService.getAllUser());
+
+    }
+    
+
+    @PostMapping("/delete")
+    public String deleteUser(@RequestBody List<UserMasterPojo> user) {
+
+        return authService.deleteUserById(user);    
+
+    }
+    
+
+    @GetMapping("/userDetails")
+    public ResponseEntity<?> getUserDetails() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        UserMasterEntity user = authService.getUserDetails(username);
+        return ResponseEntity.ok(user);
+    }
+
 }
