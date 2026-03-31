e869c4e5834e869b4d65257b61354434d9922b05
Ananthakrishna810 <ananthakrishna810@gmail.com>
2026-03-31 23:44:40 +0530

    added two more functions to fetch all the users data and and the userlist of active users

--- src/main/java/com/example/firewall/dao/UserMasterDao.java
+++ src/main/java/com/example/firewall/dao/UserMasterDao.java
@@ -3,6 +3,8 @@ package com.example.firewall.dao;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.firewall.entity.UserMasterEntity;
@@ -13,5 +15,11 @@ public interface UserMasterDao extends JpaRepository<UserMasterEntity,Long>{
     Optional<UserMasterEntity> findByUsername(String username);
 
     boolean existsByUsername(String username);
+
+    @Query("select u from UserMasterEntity u where u.username = :username and u.status = :status")
+    UserMasterEntity findUserList(@Param("username") String username,@Param("status") String status);
+
+    @Query("select u from UserMasterEntity u where u.status = :status")
+    UserMasterEntity findAllUser(@Param("status") String status);
 
 }
